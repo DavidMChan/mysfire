475303d563cd73df3c0bddb09f95e5faedb5a8d1
DavidMChan <david@iridescent.io>
2022-01-26 17:00:11 -0500

    Removed mysfire_scripts directory

--- mysfire_scripts/test_one_shot_loader.py
+++
@@ -1,4 +0,0 @@
-from mysfire import OneShotLoader
-
-ll = OneShotLoader(filepath="/home/davidchan/Projects/mysfire/mysfire_scripts/simple_data.tsv")
-print(ll([["0", "1", "hello world"], ["1", "2", "hello world 2"]]))
