07510e8e53533e15069a26b34937b5f704ddeb0e
davidchan <davidchan@whitebeard.eecs.berkeley.edu>
2022-02-09 14:30:13 -0800

    feat(mysfire/processors/nlp/tokenization_processor.py): Added simple whitespace tokenizer

--- mysfire/processors/nlp/tokenization_processor.py
+++ mysfire/processors/nlp/tokenization_processor.py
@@ -1,5 +1,9 @@
-from typing import Any, Dict, List, Optional
+from collections import Counter
+from typing import Any, Dict, List, Optional, Iterable
+from typing import Counter as CounterType
 
+
+import json
 import torch
 
 from mysfire.torch_utils import padded_stack
@@ -50,4 +54,99 @@ class HuggingfaceTokenizationProcessor(Processor):
             "text": value,
             "tokens": tokens.tokens,
             "numerized": torch.IntTensor(tokens.ids),
+        }
+
+
+class VocabTokenizationProcessor(Processor):
+    def __init__(
+        self,
+        vocab_json: str,
+        max_sequence_length: int,
+        unk_token: Optional[str] = None,
+        pad_token: Optional[str] = None,
+    ) -> None:
+        """Simple whitepsace-based tokenization using a JSON which maps strings to integers.
+
+        Args:
+            vocab_json (str): Path to a JSON file which maps strings to integers.
+            max_sequence_length (int): Maximum sequence length.
+        """
+
+        # Load the tokenizer definitions from JSON file
+        with open(vocab_json, "r") as f:
+            self._vocab = json.load(f)
+
+        # Handle some special token definitions
+        if unk_token is None:
+            unk_token = "<unk>"
+        if unk_token not in self._vocab:
+            raise ValueError(f"unk_token {unk_token} not in vocab")
+        if pad_token is None:
+            pad_token = "<pad>"
+        if pad_token not in self._vocab:
+            raise ValueError(f"pad_token {pad_token} not in vocab")
+
+        self._unk_token = unk_token
+        self._pad_token = pad_token
+
+        self._max_sequence_length = max_sequence_length
+        self._vocab_size = max(self._vocab.values()) + 1
+
+    @classmethod
+    def typestr(cls) -> str:
+        return "nlp.vocab_tokenization"
+
+    @classmethod
+    def build_vocab_from_strings(cls, outut_path: str, input_strings: Iterable[str], vocab_size: int = 10000) -> None:
+        """Build a vocab to a JSON file from a list of input strings.
+
+        Args:
+            outut_path (str): THe output path to write the JSON file to
+            input_strings (Iterable[str]): The strings to build the vocabulary from
+        """
+
+        base_tokens = {
+            "<pad>": 0,
+            "<unk>": 1,
+        }
+
+        counts: CounterType[str] = Counter()
+        n_strings = 0
+        for s in input_strings:
+            counts.update(s.split())
+            n_strings += 1
+
+        most_common = counts.most_common(vocab_size - len(base_tokens))
+        base_tokens.update({k: i + len(base_tokens) for i, (k, v) in enumerate(most_common)})
+
+        with open(outut_path, "w") as f:
+            json.dump(base_tokens, f)
+
+        print("Constructed vocab of size {} from {} strings".format(len(base_tokens), n_strings))
+
+    def collate(self, batch: List[Dict[str, Any]]) -> Dict[str, Any]:
+        return {
+            "tokens": torch.stack([x["tokens"] for x in batch], dim=0),
+            "sequence_mask": torch.stack([x["sequence_mask"] for x in batch], dim=0),
+            "text": [x["text"] for x in batch],
+        }
+
+    def __call__(self, value: str) -> Dict[str, Any]:
+
+        tokens = value.split()
+        if len(tokens) >= self._max_sequence_length:
+            tokens = tokens[: self._max_sequence_length]
+            sequence_mask = torch.ones(self._max_sequence_length)
+        else:
+            # Pad the sequence with pad tokens
+            tokens = tokens + [self._pad_token] * (self._max_sequence_length - len(tokens))
+            sequence_mask = torch.cat([torch.ones(len(tokens)), torch.zeros(self._max_sequence_length - len(tokens))])
+
+        # Convert tokens to integers
+        numerized = torch.IntTensor([self._vocab.get(t, self._vocab[self._unk_token]) for t in tokens])
+
+        return {
+            "text": tokens,
+            "tokens": numerized,
+            "sequence_mask": sequence_mask,
         }
