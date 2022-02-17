098cc576f85ce1a51b8deaa5608bda604f0a90a2
davidchan <davidchan@whitebeard.eecs.berkeley.edu>
2022-02-17 13:02:32 -0800

    fix(mysfire/vars_registry.py): Made Dataset class pickleable with cloudpickle

--- mysfire/vars_registry.py
+++ mysfire/vars_registry.py
@@ -1,8 +1,12 @@
 from .processors._processor import Processor
-from typing import Any, Sequence, Dict
+from typing import Any, Sequence, Dict, Callable, Optional
 
 
 class VarProxy:
+    # Must exist for cloudpickle to function correctly
+    _proxy_fns: Dict[str, Callable[[], Any]] = {}
+    _processor: Optional[Processor] = None
+
     def __init__(self, processor: Processor, attrs: Sequence[str]) -> None:
         self._proxy_fns = {}
         self._processor = processor
@@ -11,14 +15,16 @@ class VarProxy:
 
     def __getattr__(self, __name: str) -> Any:
         if __name in self._proxy_fns:
-            return self._proxy_fns[__name]()  # type: ignore
-        raise ValueError(
+            return self._proxy_fns[__name]()
+        raise AttributeError(
             f"""{__name} is not a variable exported by {self._processor.__class__.__name__}."""
             f""" Valid variables are: {', '.join(self._proxy_fns.keys())}"""
         )
 
 
 class VariableRegistry:
+    _proxies: Dict[str, VarProxy] = {}  # Must exist for cloudpickle to function correctly
+
     def __init__(self) -> None:
         self._proxies: Dict[str, VarProxy] = {}
 
@@ -30,7 +36,6 @@ class VariableRegistry:
     def __getattr__(self, __name: str) -> Any:
         if __name in self._proxies:
             return self._proxies[__name]
-
-        raise ValueError(
+        raise AttributeError(
             f"{__name} does not refer to a column in this dataset. Valid columns are: {', '.join(self._proxies.keys())}"
         )
